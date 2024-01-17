6faf8f153ad315ce8f2c25e45e8d84f81d7d8721
Henry Nguyen <tuo91182@temple.edu>
2024-01-17 12:38:53 -0500

    new changes

--- random.c
+++ random.c
@@ -1,2 +1,18 @@
-# hello there
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// You are to develop the function 
+// randchar() as a separate file (random.c) 
+// and include it in your repository. 
+// The function randchar() is a character function 
+// that returns a random character from ‘A’ – ‘Z’. The character is used in 
+// the main program to generate a random 7 letter word.
+
+int randchar(){
+        char letters[] = "ABCDEFGHIJKLMNPQRSTUVWXYZ"; 
+        int randomNum = rand() % 26;
+        printf("%c", letters[randomNum]);
+        return 0;
+}       
 
